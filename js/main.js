a3f088c52595a8353c4b0e203bcb4952697bb2c8
sanbragone <bragon@gmail.com>
2022-08-15 21:01:16 -0300

    Update js + codigo html

--- js/main.js
+++ js/main.js
@@ -1,339 +1,415 @@
-// Bienvenida al sitio.
-alert("Welcome to sanbragone.ar shopping site! :)");
-
-// Validacion de mayoria de edad.
-let input = parseInt(prompt("Enter your year of birth (YYYY)"));
-
-if (input <= 2004) {
-  // Si es mayor de edad le da la bienvenida al nombre ingresado.
-
-  alert("You are allowed to enter the site. Enjoy!");
-
-  let nombrePila = prompt("Enter your name");
-
-  const welcome = () => "Welcome " + nombrePila + "!";
-
-  alert(welcome());
-
-  // Sino se frena la aplicacion.
-} else {
-  alert("Site access denied due to underage :(");
-}
-
 // --------------------- ARRAYS ---------------------
 // Array para fotos de Europa
-const europe = [
+const arrayEurope = [
   {
     id: 1,
-    nombre: "eu2.jpg",
-    precio: 10,
+    nameImage: "Europe - Bridge",
+    path: "../img/trips/eu2018/eu2.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 2,
-    nombre: "eu3.jpg",
-    precio: 15,
+    nameImage: "Europe - Town",
+    path: "../img/trips/eu2018/eu4.jpg",
+    price: 15,
     stock: 1.797693134862315e308,
   },
   {
     id: 3,
-    nombre: "eu4.jpg",
-    precio: 20,
+    nameImage: "Europe - Cathedral",
+    path: "../img/trips/eu2018/eu3.jpg",
+    price: 20,
     stock: 1.797693134862315e308,
   },
   {
     id: 4,
-    nombre: "eu7.jpg",
-    precio: 10,
+    nameImage: "Europe - Another town",
+    path: "../img/trips/eu2018/eu7.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 5,
-    nombre: "eu8.jpg",
-    precio: 20,
+    nameImage: "Europe - Subway entrance",
+    path: "../img/trips/eu2018/eu8.jpg",
+    price: 20,
     stock: 1.797693134862315e308,
   },
   {
     id: 6,
-    nombre: "eu9.jpg",
-    precio: 15,
+    nameImage: "Europe - Graffity",
+    path: "../img/trips/eu2018/eu9.jpg",
+    price: 15,
     stock: 1.797693134862315e308,
   },
   {
     id: 7,
-    nombre: "eu10.jpg",
-    precio: 15,
+    nameImage: "Europe - Wall",
+    path: "../img/trips/eu2018/eu10.jpg",
+    price: 15,
     stock: 1.797693134862315e308,
   },
   {
     id: 8,
-    nombre: "eu11.jpg",
-    precio: 12,
+    nameImage: "Europe - Car",
+    path: "../img/trips/eu2018/eu11.jpg",
+    price: 12,
     stock: 1.797693134862315e308,
   },
   {
     id: 9,
-    nombre: "eu12.jpg",
-    precio: 10,
+    nameImage: "Europe - Arc of Triumph",
+    path: "../img/trips/eu2018/eu12.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 10,
-    nombre: "eu13.jpg",
-    precio: 10,
+    nameImage: "Europe - Eiffell Tower",
+    path: "../img/trips/eu2018/eu13.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 11,
-    nombre: "eu14.jpg",
-    precio: 12,
+    nameImage: "Europe - Venice canal",
+    path: "../img/trips/eu2018/eu14.jpg",
+    price: 12,
     stock: 1.797693134862315e308,
   },
   {
     id: 12,
-    nombre: "eu15.jpg",
-    precio: 10,
+    nameImage: "Europe - Burano buildings",
+    path: "../img/trips/eu2018/eu15.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 13,
-    nombre: "eu16.jpg",
-    precio: 12,
+    nameImage: "Europe - Monaco",
+    path: "../img/trips/eu2018/eu16.jpg",
+    price: 12,
     stock: 1.797693134862315e308,
   },
   {
     id: 14,
-    nombre: "eu17.jpg",
-    precio: 10,
+    nameImage: "Europe - Cristal Palace",
+    path: "../img/trips/eu2018/eu17.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 15,
-    nombre: "eu18.jpg",
-    precio: 10,
+    nameImage: "Europe - Vespa",
+    path: "../img/trips/eu2018/eu18.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
     id: 16,
-    nombre: "eu20.jpg",
-    precio: 12,
+    nameImage: "Europe - People taking photos",
+    path: "../img/trips/eu2018/eu20.jpg",
+    price: 12,
     stock: 1.797693134862315e308,
   },
 ];
 
 // Array para fotos de Mexico
-const mexico = [
+const arrayMexico = [
   {
-    id: 1,
-    nombre: "mx1.jpg",
-    precio: 30,
+    id: 17,
+    nameImage: "Mexico - Man in the beach",
+    price: 30,
+    path: "../img/trips/mx2022/mx1.jpg",
     stock: 1.797693134862315e308,
   },
   {
-    id: 2,
-    nombre: "mx2.jpg",
-    precio: 10,
+    id: 18,
+    nameImage: "Mexico - Man on a dock",
+    path: "../img/trips/mx2022/mx2.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 3,
-    nombre: "mx3.jpg",
-    precio: 15,
+    id: 19,
+    nameImage: "Mexico - Mermaid statue",
+    path: "../img/trips/mx2022/mx3.jpg",
+    price: 15,
     stock: 1.797693134862315e308,
   },
   {
-    id: 4,
-    nombre: "mx4.jpg",
-    precio: 10,
+    id: 20,
+    nameImage: "Mexico - Cenote",
+    path: "../img/trips/mx2022/mx4.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 5,
-    nombre: "mx5.jpg",
-    precio: 10,
+    id: 21,
+    nameImage: "Mexico - Bird on the pier",
+    path: "../img/trips/mx2022/mx5.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 6,
-    nombre: "mx6.jpg",
-    precio: 10,
+    id: 22,
+    nameImage: "Mexico - Aerial view of dock",
+    path: "../img/trips/mx2022/mx6.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 7,
-    nombre: "mx7.jpg",
-    precio: 10,
+    id: 23,
+    nameImage: "Mexico - Mexican food",
+    path: "../img/trips/mx2022/mx7.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 8,
-    nombre: "mx8.jpg",
-    precio: 10,
+    id: 24,
+    nameImage: "Mexico - Banner walk",
+    path: "../img/trips/mx2022/mx8.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 9,
-    nombre: "mx9.jpg",
-    precio: 20,
+    id: 25,
+    nameImage: "Mexico - Beach",
+    path: "../img/trips/mx2022/mx9.jpg",
+    price: 20,
     stock: 1.797693134862315e308,
   },
   {
-    id: 10,
-    nombre: "mx10.jpg",
-    precio: 10,
+    id: 26,
+    nameImage: "Mexico - Another beach",
+    path: "../img/trips/mx2022/mx10.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 11,
-    nombre: "mx11.jpg",
-    precio: 10,
+    id: 27,
+    nameImage: "Mexico - Feet in the water",
+    path: "../img/trips/mx2022/mx11.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 12,
-    nombre: "mx12.jpg",
-    precio: 18,
+    id: 28,
+    nameImage: "Mexico - Another beach",
+    path: "../img/trips/mx2022/mx12.jpg",
+    price: 18,
     stock: 1.797693134862315e308,
   },
   {
-    id: 13,
-    nombre: "mx13.jpg",
-    precio: 10,
+    id: 29,
+    nameImage: "Mexico - View of the beach from a tent",
+    path: "../img/trips/mx2022/mx13.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 14,
-    nombre: "mx14.jpg",
-    precio: 16,
+    id: 30,
+    nameImage: "Mexico - Another beach",
+    path: "../img/trips/mx2022/mx14.jpg",
+    price: 16,
     stock: 1.797693134862315e308,
   },
   {
-    id: 15,
-    nombre: "mx15.jpg",
-    precio: 10,
+    id: 31,
+    nameImage: "Mexico - Another beach",
+    path: "../img/trips/mx2022/mx15.jpg",
+    price: 10,
     stock: 1.797693134862315e308,
   },
   {
-    id: 16,
-    nombre: "mx16.jpg",
-    precio: 16,
-    stock: 1.797693134862315e308,
-  },
-];
-
-// Array para fotos de NYC
-const nyc = [
-  {
-    id: 1,
-    nombre: "nyc1.jpg",
-    precio: 20,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 2,
-    nombre: "nyc2.jpg",
-    precio: 35,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 3,
-    nombre: "nyc3.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 4,
-    nombre: "nyc4.jpg",
-    precio: 30,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 5,
-    nombre: "nyc5.jpg",
-    precio: 50,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 6,
-    nombre: "nyc6.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 7,
-    nombre: "nyc7.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 8,
-    nombre: "nyc8.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 9,
-    nombre: "nyc9.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 10,
-    nombre: "nyc10.jpg",
-    precio: 15,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 11,
-    nombre: "nyc11.jpg",
-    precio: 25,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 12,
-    nombre: "nyc12.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 13,
-    nombre: "nyc13.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 14,
-    nombre: "nyc14.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 15,
-    nombre: "nyc15.jpg",
-    precio: 10,
-    stock: 1.797693134862315e308,
-  },
-  {
-    id: 16,
-    nombre: "nyc16.jpg",
-    precio: 25,
+    id: 32,
+    nameImage: "Mexico - View of the beach from the jungle",
+    path: "../img/trips/mx2022/mx16.jpg",
+    price: 16,
     stock: 1.797693134862315e308,
   },
 ];
 
-// Genero un nuevo array ("trips") juntando los tres arrays y lo muestro en consola.
-const trips = europe.concat(mexico, nyc);
-console.log(trips);
+// Array para fotos de NYC (New York City)
+const arrayNYC = [
+  {
+    id: 33,
+    nameImage: "NYC - Police car",
+    path: "../img/trips/ny2019/nyc1.jpg",
+    price: 20,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 34,
+    nameImage: "NYC - UPS car",
+    path: "../img/trips/ny2019/nyc2.jpeg",
+    price: 35,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 35,
+    nameImage: "NYC - Yellow bus",
+    path: "../img/trips/ny2019/nyc3.jpeg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 36,
+    nameImage: "NYC - Cars in New York City",
+    path: "../img/trips/ny2019/nyc4.jpg",
+    price: 30,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 37,
+    nameImage: "NYC - Girl in Bew York City",
+    path: "../img/trips/ny2019/nyc5.jpg",
+    price: 50,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 38,
+    nameImage: "NYC - Girl in Bew York City",
+    path: "../img/trips/ny2019/nyc6.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 39,
+    nameImage: "NYC - United States flag",
+    path: "../img/trips/ny2019/nyc7.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 40,
+    nameImage: "NYC - People walking in New York City",
+    path: "../img/trips/ny2019/nyc8.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 41,
+    nameImage: "NYC - Man in New York City",
+    path: "../img/trips/ny2019/nyc9.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 42,
+    nameImage: "NYC - Yellow taxi",
+    path: "../img/trips/ny2019/nyc10.jpg",
+    price: 15,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 43,
+    nameImage: "NYC - Cafeteria",
+    path: "../img/trips/ny2019/nyc11.jpg",
+    price: 25,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 44,
+    nameImage: "NYC - Girl in New York City",
+    path: "../img/trips/ny2019/nyc12.jpeg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 45,
+    nameImage: "NYC - Old car parke",
+    path: "../img/trips/ny2019/nyc13.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 46,
+    nameImage: "NYC - Parked cars",
+    path: "../img/trips/ny2019/nyc14.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 47,
+    nameImage: "NYC - People walking in the night",
+    path: "../img/trips/ny2019/nyc15.jpg",
+    price: 10,
+    stock: 1.797693134862315e308,
+  },
+  {
+    id: 48,
+    nameImage: "NYC - People walking in the night",
+    path: "../img/trips/ny2019/nyc16.jpg",
+    price: 25,
+    stock: 1.797693134862315e308,
+  },
+];
 
-// Parseo el array "trips" y muestro en consola nombre y precio de todos los objetos.
-for (const nombre of trips) {
-  console.log(nombre.nombre);
-  console.log(nombre.precio);
+// Funcion con codigo HTML para ir agregando las cards a la web.
+
+const productToHTML = (product) => {
+  const HTMLCode = `
+  <div class="col-sm-6 col-lg-3">
+    <div class="card" style="width: 30rem;">
+        <img src=${product.path} class="card-img-top">
+        <div class="card-body">
+          <h4 class="card-title">${product.nameImage}</h4>
+          <h5 class="card-title">Price: U$S ${product.price}</h5>
+          <a href="#" id="button-${product.id}" class="btn btn-dark">Add to cart</a>
+        </div>
+    </div>
+  </div>
+  `;
+  return HTMLCode;
+};
+
+// Funcion e iteracion para agregar cards de fotos de Europa a la web.
+
+const addToCatalogEurope = (productEurope) => {
+  const catalogEurope = document.getElementById("catalogEurope");
+  catalogEurope.innerHTML += productToHTML(productEurope);
+};
+
+for (const productEurope of arrayEurope) {
+  addToCatalogEurope(productEurope);
 }
 
-// Veo la longitud del nuevo array "trips" (48 objetos) y lo muestro en consola para control interno.
-console.log("El array TRIPS tiene " + trips.length + " objetos en total");
+// Funcion e iteracion para agregar cards de fotos de Mexico a la web.
 
-// Filtro para precio menor a 25 dolares.
-const filtroPrecio = trips.filter((el) => el.precio < 25);
-console.log(filtroPrecio);
+const addToCatalogMexico = (productMexico) => {
+  const catalogMexico = document.getElementById("catalogMexico");
+  catalogMexico.innerHTML += productToHTML(productMexico);
+};
+
+for (const productMexico of arrayMexico) {
+  addToCatalogMexico(productMexico);
+}
+
+// Funcion e iteracion para agregar cards de fotos de NYC a la web.
+
+const addToCatalogNYC = (productNYC) => {
+  const catalogNYC = document.getElementById("catalogNYC");
+  catalogNYC.innerHTML += productToHTML(productNYC);
+};
+
+for (const productNYC of arrayNYC) {
+  addToCatalogNYC(productNYC);
+}
+
+const arrayTripsAll = arrayEurope.concat(arrayMexico, arrayNYC);
+
+// Array vacio para ir agregando productos.
+
+const shoppingCart = [];
+
+// Para carro de compras.
+
+for (const product of arrayTripsAll) {
+  const button = document.getElementById(`button-${product.id}`);
+  button.addEventListener("click", () => {
+    shoppingCart.push(product);
+    console.log("El carrito de compras tiene " + shoppingCart.length + " productos.");
+  });
+}
