425f760209c02f740a4bf0f30e739a54f7b5c42b
sanbragone <bragon@gmail.com>
2022-09-06 11:05:49 -0300

    Implementacion API

--- js/main.js
+++ js/main.js
@@ -1,12 +1,134 @@
-// Variable para contador de carrito.
+// Fetch to retrieve information from internal JSON (simulating external API).
+
+fetch("../json/sanbragoneArray.json")
+  .then((response) => response.json())
+  .then((dataArray) => {
+
+    // Function and iteration to display the catalogs (HTML code - cards) of each trip on the web page.
+
+    const showCatalog = () => {
+      const catalogNode = document.getElementById("catalog");
+      let catalogHTML = "";
+
+      for (const product of dataArray) {
+        catalogHTML += productToHTML(product);
+      }
+
+      catalogNode.innerHTML = catalogHTML;
+      buttonCatalog();
+    };
+
+    // Function and iteration to display the products in the cart (HTML code - cards) on the web page.
+
+    const showCart = () => {
+      const cartNode = document.getElementById("totalCart");
+      const priceNode = document.getElementById("totalAmount");
+
+      let cartHTML = "";
+      let price = 0;
+      for (const product of shoppingCart) {
+        cartHTML += cartToHTML(product);
+        price += product.price;
+      }
+
+      cartNode.innerHTML = cartHTML;
+      priceNode.innerHTML = price;
+
+      buttonCart();
+    };
+
+    // Functions and event that listens when a button of any of the cards of any of the arrays is clicked.
+
+    const buttonCatalog = () => {
+      for (const product of dataArray) {
+        const buttonCatalog = document.getElementById(
+          `button-catalog-${product.id}`
+        );
+
+        buttonCatalog.addEventListener("click", () => {
+          const productCart = {
+            nameImage: product.nameImage,
+            price: product.price,
+            idPurchase: counterCart,
+          };
+
+          // Popup (from Toastify library) for confirmation of photo added to cart.
+          Toastify({
+            text: "Photo added to cart :D",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "black",
+            },
+            onClick: function () {}, // Callback after click
+          }).showToast();
+
+          counterCart += 1;
+          shoppingCart.push(productCart);
+          showCart();
+          localStorage.setItem("itemCart", JSON.stringify(shoppingCart));
+        });
+      }
+    };
+
+    const buttonCart = () => {
+      for (const product of shoppingCart) {
+        const buttonCart = document.getElementById(
+          `button-cart-${product.idPurchase}`
+        );
+
+        buttonCart.addEventListener("click", () => {
+          const index = shoppingCart.findIndex(
+            (p) => p.idPurchase == product.idPurchase
+          );
+
+          // Popup (from Toastify library) for confirmation of photo removed from cart.
+          Toastify({
+            text: "Photo removed from cart :(",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "black",
+            },
+            onClick: function () {}, // Callback after click
+          }).showToast();
+
+          shoppingCart.splice(index, 1);
+          showCart();
+          localStorage.removeItem("itemCart");
+        });
+      }
+    };
+
+    // Execution of function to add to catalog.
+
+    showCatalog();
+
+    // It retrieves the information from the localStorage and validates if the cart has items to present them again on the web. For this I use the logical operator OR.
+    const shoppingCart = JSON.parse(localStorage.getItem("itemCart")) || [];
+    showCart();
+
+    // A ternary operator is used to show whether the LS is empty or full. Based on that I display a message or the array elements.
+    shoppingCart != 0
+      ? console.log(shoppingCart)
+      : console.log("El localStorage está vacío.");
+  });
+
+// Variable for cart counter.
 
 let counterCart = 0;
 
-// Array vacio para ir agregando productos.
-
-//let shoppingCart = [];
-
-// Funcion con codigo HTML para ir agregando las cards de los arreglos que tengo declarados, a la web.
+// Function with HTML code to add the cards from the arrays that I have declared, to the web.
 
 const productToHTML = (product) => {
   return `
@@ -23,7 +145,7 @@ const productToHTML = (product) => {
   `;
 };
 
-// Funcion con codigo HTML para ir agregando las cards de lo que se va agregando al carrito, a la web (carrito de compras).
+// Function with HTML code to add the cards of what is being added to the cart, to the web (shopping cart).
 
 const cartToHTML = (product) => {
   return `
@@ -38,132 +160,3 @@ const cartToHTML = (product) => {
   </div>
   `;
 };
-
-// Funcion e iteracion para mostrar los catalogos (codigo HTML - cards) de cada viaje en la pagina web.
-
-const showCatalog = () => {
-  const catalogNode = document.getElementById("catalog");
-  let catalogHTML = "";
-
-  for (const product of photos) {
-    catalogHTML += productToHTML(product);
-  }
-
-  catalogNode.innerHTML = catalogHTML;
-  buttonCatalog();
-};
-
-// Funcion e iteracion para mostrar los productos del carrito (codigo HTML - cards) en la pagina web.
-
-const showCart = () => {
-  const cartNode = document.getElementById("totalCart");
-  const priceNode = document.getElementById("totalAmount");
-
-  let cartHTML = "";
-  let price = 0;
-  for (const product of shoppingCart) {
-    cartHTML += cartToHTML(product);
-    price += product.price;
-  }
-
-  cartNode.innerHTML = cartHTML;
-  priceNode.innerHTML = price;
-
-  buttonCart();
-};
-
-// Funciones y evento que escucha cuando se hace clic sobre un boton de alguna de las cards de cualquiera de los arrays.
-
-const buttonCatalog = () => {
-  for (const product of photos) {
-    const buttonCatalog = document.getElementById(
-      `button-catalog-${product.id}`
-    );
-
-    buttonCatalog.addEventListener("click", () => {
-      const productCart = {
-        nameImage: product.nameImage,
-        price: product.price,
-        idPurchase: counterCart,
-      };
-
-      // Popup (de libreria Toastify) para confirmacion de fotografia agregada al carrito.
-      Toastify({
-        text: "Photo added to cart :D",
-        duration: 3000,
-        destination: "https://github.com/apvarun/toastify-js",
-        newWindow: true,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "black",
-        },
-        onClick: function () {}, // Callback after click
-      }).showToast();
-
-      counterCart += 1;
-      shoppingCart.push(productCart);
-      showCart();
-      localStorage.setItem("itemCart", JSON.stringify(shoppingCart));
-    });
-  }
-};
-
-const buttonCart = () => {
-  for (const product of shoppingCart) {
-    const buttonCart = document.getElementById(
-      `button-cart-${product.idPurchase}`
-    );
-
-    buttonCart.addEventListener("click", () => {
-      const index = shoppingCart.findIndex(
-        (p) => p.idPurchase == product.idPurchase
-      );
-
-      // Popup (de libreria Toastify) para confirmacion de fotografia eliminada del carrito.
-      Toastify({
-        text: "Photo removed from cart :(",
-        duration: 3000,
-        destination: "https://github.com/apvarun/toastify-js",
-        newWindow: true,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "black",
-        },
-        onClick: function () {}, // Callback after click
-      }).showToast();
-
-      shoppingCart.splice(index, 1);
-      showCart();
-      localStorage.removeItem("itemCart");
-    });
-  }
-};
-
-// Ejecucion de funcion para agregar a catalogo.
-
-showCatalog();
-
-// Recupero la informacion del localStorage y valido si el carrito tiene items para presentarlos nuevamente en la web. Para esto utilizo el operador logico OR.
-const shoppingCart = JSON.parse(localStorage.getItem("itemCart")) || [];
-showCart();
-
-// Utilizo operador ternario para mostrar si el LS esta vacio o lleno. En base a eso muestro un mensaje o los elementos del arreglo.
-shoppingCart != 0
-  ? console.log(shoppingCart)
-  : console.log("El localStorage está vacío.");
-
-
-// Desestructuracion de arreglo "photos".
-const [a, , , , , b] = photos;
-
-console.log(a);
-console.log(b);
-
-// Spread de array.
-console.log(...photos);
